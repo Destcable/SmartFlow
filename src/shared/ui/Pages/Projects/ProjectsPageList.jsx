2b9d5afcf0aa5595bd3b3eabf783adcaf45da746
Destcable <artmobpavlov21@gmail.com>
2024-07-26 03:44:10 +0300

    update Projects page(add Sidebar, add content)

--- src/shared/ui/Pages/Projects/ProjectsPageList.jsx
+++ src/shared/ui/Pages/Projects/ProjectsPageList.jsx
@@ -1,6 +1,7 @@
-import { makeStyles } from '@fluentui/react-components'
+import { Avatar, Body1Strong, Button, makeStyles } from '@fluentui/react-components'
 import { Sidebar } from '../../Sidebar/Sidebar';
 import { SidebarProjects } from './components/SidebarProjects';
+import { StarRegular } from '@fluentui/react-icons';
 
 const useStyles = makeStyles({
     sidebar: {
@@ -25,12 +26,37 @@ const useStyles = makeStyles({
     }
 });
 
+const useStylesContentProjectsList = makeStyles({ 
+    contentHeader: { 
+        display: 'flex', 
+        alignItems: 'center',
+        gap: '10px',
+        backgroundColor: 'rgba(255,255,255,255)', 
+        padding: '1rem', 
+        borderBottom: `1px solid #e1e1e1`,
+    },
+    avatarProject: { 
+        borderRadius: '8px',
+    }
+})
+
 export const ProjectsPageList = () => {
     const styles = useStyles()
+    const stylesContent = useStylesContentProjectsList()
 
-    return <Sidebar>
+    return <div style={{ display: 'flex'}}>
+    <Sidebar>
         <SidebarProjects styles={styles}/>
     </Sidebar>
+    <div style={{ backgroundColor: 'rgba(244,245,247,255)', width: '100%'}}>
+        <div className={stylesContent.contentHeader}>
+            <Button appearance='transparent' icon={<StarRegular />}/>
+            <Avatar className={stylesContent.avatarProject} name="TEST | HR App MVP"/>
+            <Body1Strong>TEST | HR App MVP</Body1Strong>
+        </div>
+    </div>
+    </div>
+
 }
 
 
